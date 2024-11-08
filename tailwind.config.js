a26eda1b6da449d0cd70c65bb6983b96ccb4dbcb
roadmaus <gufe@protonmail.com>
2024-11-08 01:14:32 +0100

    update README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    './Topics/**/*.{ts,tsx}',
+    './topics/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
